b7aa9bf5796f851c6182fa529af02a198b6f2bfb
menalars <33983370+menalars@users.noreply.github.com>
2019-06-01 19:21:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var n1 = 0
+var n2 = 0
+
+function solucao(n1, n2, ) {
+    if (document.getElementById('adição').checked) {
+        var n1 = Number(document.getElementById('n1').value)
+        var n2 = Number(document.getElementById('n2').value)
+        var resposta = n1 + n2;
+        if (isNaN(resposta)){
+            alert("Sabe usar número não, é? >:C")
+        } else {
+            document.getElementById('resposta').innerHTML = `O resultado da operação é ${resposta} `
+        }    
+    } else if (document.getElementById('subtração').checked) {
+        var n1 = Number(document.getElementById('n1').value)
+        var n2 = Number(document.getElementById('n2').value)
+        var resposta = n1 - n2;if (isNaN(resposta)){
+            alert("Sabe usar número não, é? >:C")
+        } else {
+            document.getElementById('resposta').innerHTML = `O resultado da operação é ${resposta} `
+        }  
+    } else if (document.getElementById('multiplicação').checked) {
+        var n1 = Number(document.getElementById('n1').value)
+        var n2 = Number(document.getElementById('n2').value)
+        var resposta = n1 * n2;if (isNaN(resposta)){
+            alert("Sabe usar número não, é? >:C")
+        } else {
+            document.getElementById('resposta').innerHTML = `O resultado da operação é ${resposta} `
+        }  
+    } else if (document.getElementById('divisão').checked) {
+        var n1 = Number(document.getElementById('n1').value)
+        var n2 = Number(document.getElementById('n2').value)
+        var resposta = n1 / n2;if (isNaN(resposta)){
+            alert("Sabe usar número não, é? >:C")
+        } else {
+            document.getElementById('resposta').innerHTML = `O resultado da operação é ${resposta} `
+        }  
+    } else {
+        alert("Sabe usar número não, é? >:C")
+    }
+}
+
+
